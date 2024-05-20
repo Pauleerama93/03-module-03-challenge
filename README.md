40d65a4d9512f7d621823a4336927df347c68cf5
rpaulnelsonii@gmail.com <rpaulnelsonii@gmail.com>
2024-05-19 20:48:16 -0700

    Edited script.js file to intake employee names and salary, get average of the salaries, and generate a random employee in console, Created a Read Me file, Created Image folder and added screen shots of site.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# 03-module-03-challenge
-Module 03 Challenge
+# Employee Payroll Tracker
+This website tracks employee names, salary, and average salaries of all listed employees. It can also generate a random employee in the console if you would like to set up an employee lottery!
+
+## Features
+-**Employee Roster:** A roster that displays a table with employee's first and last name with their personal salary.
+
+-**Add Employee Button:** A button that allows you to add Employee's to the roster.
+
+-**Random Employee Generator:** A random employee genertator located in the concole.
+
+## Technologies Used
+**Frontend:**- CSS and HTML
+
+**Backend:**- JavaScript
+
+## Website Links
+
+**Deployed Site:**
+
+**Google Doc:** https://docs.google.com/document/d/1gSOGwC9D6FRdNbrgqun1FucEKbeT93dE77jjjNvJTkQ/edit?usp=sharing 
+I set up a todo with notes on how to complete the tasks in a google docs. You're more than welcome to check it out!
+
+
+## Screenshots
+![Home Page](./assets/images/HomePage.png)
+![Filled Out Page](./assets/images/FilledOutPage.png)
+![Console Page](./assets/images/ConsolePage.png)
+
+## Credits
+
+**UC Berkley's Bootcamp Staff:** Drew Hoang and Kyle Vance for all their support and guidence. 
+
+**Starter Code:** Xander Rapstine (https://github.com/Xandromus) provided the stater code located on GitHub.
+
+## User Story 
+AS A payroll manager
+I WANT AN employee payroll tracker
+SO THAT I can see my employees' payroll data and properly budget for the company
+
+**Acceptance Criteria:**
+GIVEN an employee payroll tracker
+WHEN I click the "Add employee" button
+THEN I am presented with a series of prompts asking for first name, last name, and salary
+WHEN I finish adding an employee
+THEN I am prompted to continue or cancel
+WHEN I choose to continue
+THEN I am prompted to add a new employee
+WHEN I choose to cancel
+THEN my employee data is displayed on the page sorted alphabetically by last name, and the console shows computed and aggregated data
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,42 +4,68 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  //Creating a varible to hold the values we are going to collect for employees
   const employees = [];
-  const salaries = [];
-
+  // setting up a varible for a while loop and setting it to true so the loop will run
   let employeeFunction = true;
-
+  // A while loop to get input frim the user using prompts
   while(employeeFunction) {
     const firstName = prompt("Enter First Name: ");
     const lastName = prompt("Enter Last Name: ");
     let salary = prompt("Enter Salary: ")
-
+    // Setting a fail safe just in case the user inputs a value that isn't a number
     if (isNaN(salary)){
       salary = 0;
     }
+    // setting up a varible with multipule objects to store all of our data gathered from the user
     const employee = {
       firstName: firstName,
       lastName: lastName,
-      salary: salary
+      salary: parseInt(salary)
     }
+    //Attatching the input gathered into the employees arrary using the push technique 
     employees.push(employee);
-    salaries.push(salary);
 
-
+    //Setting up a way to end the  by using the confirm
     employeeFunction = confirm("Add Another Employee? ");
   }
-  console.log(employees, salaries);
-  return 
+   //sorting the data alphabetically
+   employees.sort();
+  //Logging the employees data as a reference to see if our code is doing what we want it to
+  console.log(employees);
+  // Ending the function and returning the vaules to the function caller
+  return employees;
 }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+  // creating a place holder for the for loop that will take all the salaries values and store them
+  let totalSalary= 0;
+  //Creating the for loop to obtain all the salaries
+  for(index = 0; index < employeesArray.length; index++){
+    totalSalary += employeesArray[index].salary;
+  }
+  //Putting in a log to check the total salary that is inputed 
+  console.log("Total Salary: " + totalSalary );
+
+  //creating a varible to store the average salary in
+  const salaryAverage = totalSalary / employeesArray.length;
+  //logging the salary to make sure it is being computed correctly 
+  console.log("Salary Average: " + salaryAverage);
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  if(employeesArray.length > 1){
+  //Creating a varible to store the random employee into
+  const randomEmployee = employeesArray[Math.round(Math.random() * employeesArray.length)]
+  console.log(`Random Employee: ${randomEmployee.firstName} ${randomEmployee.lastName}`)
+  }
+  else{
+    console.log("Not enough inputs to generate random employee")
+  }
 }
 
 /*
